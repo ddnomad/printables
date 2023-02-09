15918799b52c2d3fb47150c58580ea9250468274
ddnomad <self@ddnomad.net>
2023-02-09 01:22:45 +0000

    Resolve #5

--- utils/gen_model_stl.sh
+++ utils/gen_model_stl.sh
@@ -20,17 +20,30 @@ function main {
     local output_file_path
     output_file_path="$2"
 
+    if [[ "${output_file_path}" != *.stl ]]; then
+        >&2 echo "---(X) Error: Output file path must have .stl extension: ${output_file_path}"
+        exit 1
+    fi
+
     if ! test -f "${target_file_path}"; then
         >&2 echo "---(X) Error: Target file path does not exist or is not a regular file: ${target_file_path}"
         exit 1
     fi
 
-    "${OPENSCAD_BIN_PATH}" \
-        -o "${output_file_path}" \
-        --autocenter \
-        --imgsize 1028,512 \
-        --viewall \
-        "${target_file_path}"
+    >&2 echo "---(i) INFO: Generating model STL (this may take a while)"
+
+    local start_ts
+    start_ts="$(date +%s)"
+
+    "${OPENSCAD_BIN_PATH}" -o "${output_file_path}" "${target_file_path}"
+
+    local end_ts
+    end_ts="$(date +%s)"
+
+    local elapsed_seconds
+    elapsed_seconds=$((end_ts - start_ts))
+
+    >&2 echo "---(+) DONE: Generated model STL in ${elapsed_seconds} seconds: ${output_file_path}"
 }
 
 
