0ba91ea7d9eb2cc970df82df9291a3c70de16ada
ddnomad <self@ddnomad.net>
2023-02-11 12:39:25 +0000

    Fix fix fix

--- utils/create_model_github_release.sh
+++ utils/create_model_github_release.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCRIPT_PATH="$(dirname "$0")"
+readonly SCRIPT_PATH
 
 function main {
-    if test "$#" -ne 4; then
-        >&2 echo 'Usage: '"$(basename "$0")"' MODEL_PATH MODEL_NAME RELEASE_VERSION RELEASE_CHANGELOG_BASE64'
+    if test "$#" -ne 3; then
+        >&2 echo 'Usage: '"$(basename "$0")"' MODEL_PATH MODEL_NAME RELEASE_VERSION'
         exit 1
     fi
 
@@ -18,7 +20,7 @@ function main {
     release_version="$3"
 
     local release_changelog
-    release_changelog="$(base64 -d <<<"$4")"
+    release_changelog="$("${SCRIPT_PATH}/get_changelog_section.sh" "${model_path}/CHANGELOG.md" "${release_version}")"
 
     echo "Release model path: ${model_path}"
     echo "Release model name: ${model_name}"
